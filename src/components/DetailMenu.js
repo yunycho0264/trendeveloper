1ec42363d3d78cf7aea8ced9b178d6ff0ec5ba19
y0c0y <kkjsn1234@naver.com>
2023-05-23 14:26:01 +0900

    상세 요건, 기업트렌드 연동

--- src/components/DetailMenu.js
+++ src/components/DetailMenu.js
@@ -2,13 +2,16 @@ import React from "react";
 import styles from "../css/DetailMenu.module.css";
 import { Link } from "react-router-dom";
 
-const DetailMenu = () => {
+const DetailMenu = (props) => {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  console.log(urlSearchParams.get("id"));
+  let recruitmentID = urlSearchParams.get("id");
   return (
     <div className={styles["detail-menu"]}>
       {/*상세 요건*/}
       <div className={styles.requirement}>
         <Link
-          to="/recruitement/DetailPage/:id?"
+          to={`/recruitement/DetailPage?id=${recruitmentID}`}
           className={styles.nav}
           id="nav1"
         >
@@ -17,7 +20,11 @@ const DetailMenu = () => {
       </div>
       {/*기업 트렌드*/}
       <div className={styles["company-trend"]}>
-        <Link to="/company/detail/:id?" className={styles.nav} id="nav1">
+        <Link
+          to={`/company/detail?id=${recruitmentID}`}
+          className={styles.nav}
+          id="nav1"
+        >
           기업 트렌드
         </Link>
       </div>
