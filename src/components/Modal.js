73316c408dc887c7200468214b5ea21fa3bd9dcc
y0c0y <kkjsn1234@naver.com>
2023-05-31 08:21:24 +0900

    nodal 내용 수정

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 export const ModalContainer = styled.span`
   // Modal을 구현하는데 전체적으로 필요한 CSS를 구현
   display: flex;
+  color: black;
   justify-content: center;
   align-items: center;
   margin-top: 4vh;
@@ -56,13 +57,13 @@ export const ModalView = styled.div.attrs((props) => ({
   align-items: center;
   flex-direction: column;
   border-radius: 20px;
-  width: 500px;
-  height: 200px;
+  width: 1000px;
+  height: 800px;
   background-color: #ffffff;
   > div.desc {
     margin: 50px;
-    font-size: 20px;
-    color: var(--coz-purple-600);
+    font-size: 15px;
+    color: black;
   }
 `;
 
@@ -96,8 +97,31 @@ const HowRank = () => {
           <ModalView onClick={(e) => e.stopPropagation()}>
             <ExitBtn onClick={openModalHandler}>x</ExitBtn>
             <div className="desc">
-              직군과 연관된 가중 평균(70% (수강한 강의 평점 * 가중치) ) + 수강한
-              강의들의 수 (15%) + 공모전 및 프로젝트 경험 (15%)
+              <h3>[점수 환산 방식]</h3>
+              <p style={{ height: "20px" }} />
+              100 = 70 (평점 가중 평균) + 15 (수강 강의 수) + 15 (기술 활용
+              경험)
+              <p style={{ height: "20px" }} />
+              <h4>평점 가중 평균</h4>
+              <p style={{ height: "5px" }} />
+              세 가지 항목을 기준으로 가중치 계산.
+              <p style={{ height: "5px" }} />
+              기본 점수 (+10) : 직군과 과목이 기본적으로 연관이 되어있는가
+              <br />
+              핵심 지식 (+5) : 과목이 직업에 직접 적용할 수 있는 기본 개념과
+              원칙을 제공하는지
+              <br />
+              실용 기술 (+5) : 과목이 직군에 필요한 실용 기술을 학생들에게
+              제공하는가
+              <p style={{ height: "20px" }} />
+              <h4>수강 강의 수</h4>
+              <p style={{ height: "5px" }} />
+              과목 수에 따라 15점 만점으로 환산 (수강 과목 6개 이상 15점 만점
+              처리)
+              <p style={{ height: "20px" }} />
+              <h4>기술 활용 경험</h4>
+              <p style={{ height: "5px" }} />
+              경험 1회 당 5점, 15점 만점으로 환산 (경험 3회 이상 15점 만점 처리)
             </div>
           </ModalView>
         </ModalBackdrop>
