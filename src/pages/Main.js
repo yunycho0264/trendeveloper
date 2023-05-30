5a5432f47b4dec8cb79bd5c9b8ca82b650a5fa91
Hippoya <hanah9082@naver.com>
2023-05-30 21:48:26 +0900

    2023-05-30

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -7,7 +7,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 //배너 이미지 삽입
 import image1 from "../img/앱서비스.png";
 import image2 from "../img/Cloud.png";
-import image3 from "../img/임시배너-002.png";
+import image3 from "../img/공공데이터.png";
 
 //기업 로고 이미지 삽입
 import SimpleSlider from "../components/Slider";
@@ -45,13 +45,26 @@ const Main = () => {
       <div className={styles["banner-image"]}>
         <Carousel autoPlay interval={5000} infiniteLoop>
           <div>
-            <img src={image1} alt="Slide 1" />
+            <a href="http://obigo.com/promotion/?vid=2" target="_blank">
+              <img src={image1} alt="Slide 1" />
+            </a>
           </div>
           <div>
-            <img src={image2} alt="Slide 2" />
+            <a
+              href="https://ncamp.kr/
+"
+              target="_blank"
+            >
+              <img src={image2} alt="Slide 2" />
+            </a>
           </div>
           <div>
-            <img src={image3} alt="Slide 3" />
+            <a
+              href="https://data.seoul.go.kr/together/notice/boardView.do?seq=4eab5b7659157836591d1ac6cb966b63&pageIndex=1#none"
+              target="_blank"
+            >
+              <img src={image3} alt="Slide 3" />
+            </a>
           </div>
         </Carousel>
       </div>
